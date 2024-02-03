ed2b2273e7cc34236ead18e273043e2c4a2ec5ae
MustBeAlive <petralexd3@gmail.com>
2024-02-03 22:13:12 +0300

    modified:   scripts/module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,7 @@
 Hooks.once('init', async function() {
-
+    registerSettings();
 });
 
 Hooks.once('ready', async function() {
-
+    if (game.settings.get('mba-premades', 'Check For Updates')) checkUpdate()
 });
