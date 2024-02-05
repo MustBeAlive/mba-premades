7b80634ac60dd7204cfcc517c86d9ee06be23c2c
MustBeAlive <petralexd3@gmail.com>
2024-02-05 14:41:11 +0300

    yep

--- scripts/module.js
+++ scripts/module.js
@@ -1,12 +1,15 @@
 import {addActions} from './macros/actions/token.js';
 import {checkUpdate} from './update.js';
 import {registerSettings} from './settings.js';
+import {rollModeChange, createRollModeButtons} from './buttons.mjs';
 Hooks.once('init', () => {
     registerSettings();
+    ChatLog._setRollMode = rollModeChange;
 });
 Hooks.once('ready', async function() {
     if (game.user.isGM) {
         if (game.settings.get('mba-premades', 'Check For Updates')) checkUpdate();
     }
     Hooks.on('createToken', addActions);
+    Hooks.on('renderChatLog', createRollModeButtons);
 });
