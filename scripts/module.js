6350b501e627c3f61d83ee3814a54b2e2d17a099
MustBeAlive <petralexd3@gmail.com>
2024-02-05 04:06:21 +0300

    yep?

--- scripts/module.js
+++ scripts/module.js
@@ -1,9 +1,12 @@
 import {addActions} from './macros/actions/token.js';
 import {checkUpdate} from './update.js';
+import {rollModeChange, createRollModeButtons} from './rollButtons.js';
 import {registerSettings} from './settings.js';
 Hooks.once('init', () => {
     registerSettings();
+    ChatLog._setRollMode = rollModeChange;
 });
+Hooks.on('renderChatLog', createRollModeButtons);
 Hooks.once('ready', async function() {
     if (game.user.isGM) {
         if (game.settings.get('mba-premades', 'Check For Updates')) checkUpdate();
---
+++ scripts/rollButtons.js
@@ -0,0 +1,76 @@
+const CFG = { module: 'mba-premades' };
+
+const buttons = [
+	{
+		rollmode: 'publicroll',
+		icon: ['fas', 'fa-dice-d20'],
+		label: 'CHAT.RollPublic'
+	},
+	{
+		rollmode: 'gmroll',
+		icon: ['fas', 'fa-crown'],
+		label: 'CHAT.RollPrivate'
+	}, // 'fa-user-secret'
+	{
+		rollmode: 'blindroll',
+		icon: ['fas', 'fa-eye-slash'],
+		label: 'CHAT.RollBlind'
+	},
+	{
+		rollmode: 'selfroll',
+		icon: ['fas', 'fa-ghost'],
+		label: 'CHAT.RollSelf'
+	},
+];
+
+function changeMode(_event) {
+	game.settings.set('core', 'rollMode', this.dataset.rollmode)
+}
+
+/**
+ * @param {ChatLog} log
+ * @param {JQuery} jq
+ * @param {Object} options
+ */
+function createRollModeButtons(log, jq, options) {
+	const html = jq[0];
+	const controls = html.querySelector('#chat-controls');
+	if (!controls) return;
+	controls.classList.add('rollmode-buttons-override');
+	const rollmode = controls.querySelector('.roll-type-select');
+	if (!rollmode) return;
+
+	const rollmodeBox = document.createElement('div');
+	rollmodeBox.classList.add('rollmode-buttons');
+
+	const currentMode = game.settings.get('core', 'rollMode');
+	for (const b of buttons) {
+		const button = document.createElement('button');
+		button.dataset.rollmode = b.rollmode;
+		const icon = document.createElement('i');
+		icon.classList.add(...b.icon);
+		button.append(icon);
+		rollmodeBox.append(button);
+		b.element = button;
+		button.classList.toggle('active', currentMode === b.rollmode);
+
+		button.dataset.tooltip = b.label;
+		button.dataset.tooltipDirection = 'UP';
+
+		button.addEventListener('click', changeMode);
+	}
+
+	rollmode.after(rollmodeBox);
+	rollmode.remove();
+}
+
+function rollModeChange(mode) {
+	for (const button of buttons)
+		button.element.classList.toggle('active', mode === button.rollmode);
+}
+
+Hooks.once('init', () => {
+	ChatLog._setRollMode = rollModeChange;
+});
+
+Hooks.on('renderChatLog', createRollModeButtons);
