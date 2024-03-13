aa6d1bdcb205c219be2409e41485d7340331cbc5
MustBeAlive <petralexd3@gmail.com>
2024-03-13 18:08:53 +0300

    0.0.51

--- scripts/macros/spells/level1/charmPerson.js
+++ scripts/macros/spells/level1/charmPerson.js
@@ -1,7 +1,7 @@
-async function onCast({speaker, actor, token, character, item, args, scope, workflow}) {
+async function cast({speaker, actor, token, character, item, args, scope, workflow}) {
     let ammount = workflow.castData.castLevel;
     if (workflow.targets.size > ammount) {
-        let selection = await chrisPremades.helpers.selectTarget(workflow.item.name, chrisPremades.constants.okCancel, Array.from(workflow.targets), true, 'multiple', undefined, false, 'Too many targets selected. Choose which targets to keep (Max: ' + ammount + ')');
+        let selection = await chrisPremades.helpers.selectTarget(workflow.item.name, chrisPremades.constants.okCancel, Array.from(workflow.targets), false, 'multiple', undefined, false, 'Too many targets selected. Choose which targets to keep (Max: ' + ammount + ')');
         if (!selection.buttons) {
             ui.notifications.warn('Failed to select right ammount of targets, try again!')
             return;
@@ -9,7 +9,7 @@ async function onCast({speaker, actor, token, character, item, args, scope, work
         let newTargets = selection.inputs.filter(i => i).slice(0, ammount);
         await chrisPremades.helpers.updateTargets(newTargets);
     }
-    let targets = Array.from(game.user.targets);
+    await warpgate.wait(100);
     let featureData = await chrisPremades.helpers.getItemFromCompendium('mba-premades.MBA Spell Features', 'Charm Person: Charm', false);
     if (!featureData) {
         ui.notifications.warn('Can\'t find item in compenidum! (Charm Person: Charm)');
@@ -20,8 +20,11 @@ async function onCast({speaker, actor, token, character, item, args, scope, work
     featureData.system.save.dc = chrisPremades.helpers.getSpellDC(originItem);
     setProperty(featureData, 'chris-premades.spell.castData.school', originItem.system.school);
     let feature = new CONFIG.Item.documentClass(featureData, {'parent': workflow.actor});
+    let targets = Array.from(game.user.targets);
     let targetUuids = [];
-    for (let i of targets) targetUuids.push(i.document.uuid);
+    for (let i of targets) {
+        targetUuids.push(i.document.uuid);
+    }
     let [config, options] = chrisPremades.constants.syntheticItemWorkflowOptions(targetUuids);
     await MidiQOL.completeItemUse(feature, config, options);
 }
@@ -103,9 +106,10 @@ async function save({speaker, actor, token, character, item, args, scope, workfl
         }
     }
     targets.forEach(loop);
+    await warpgate.wait(100);
 }
 
 export let charmPerson = {
-    'onCast': onCast,
+    'cast': cast,
     'save': save
 }
