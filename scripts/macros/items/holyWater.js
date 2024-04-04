771f6cff4fd9e20b11f326d95e3ff9558ef46270
MustBeAlive <petralexd3@gmail.com>
2024-04-04 03:17:18 +0300

    0.0.73

--- scripts/macros/items/holyWater.js
+++ scripts/macros/items/holyWater.js
@@ -1,7 +1,6 @@
 export async function holyWater({speaker, actor, token, character, item, args, scope, workflow}) {
-    if (workflow.hitTargets.size != 1) return;
-    let targets = Array.from(workflow.targets);
-    let target = targets[0];
+    if (!workflow.hitTargets.size) return;
+    let target = workflow.targets.first();
     let type = chrisPremades.helpers.raceOrType(target.actor);
     if (type != 'fiend' && type != 'undead') {
         ui.notifications.warn('Target is not fiend or undead!');
@@ -9,5 +8,11 @@ export async function holyWater({speaker, actor, token, character, item, args, s
     };
     let damageFormula = '2d6[radiant]';
     let damageRoll = await new Roll(damageFormula).roll({'async': true});
-    await chrisPremades.helpers.applyWorkflowDamage(workflow.token, damageRoll, 'radiant', targets, workflow.item.name, workflow.itemCardId);
+    await MidiQOL.displayDSNForRoll(damageRoll, 'damageRoll');
+    damageRoll.toMessage({
+        rollMode: 'roll',
+        speaker: { 'alias': name },
+        flavor: `<b>Holy Water</b>`
+    });
+    await chrisPremades.helpers.applyWorkflowDamage(workflow.token, damageRoll, 'radiant', [target], workflow.item.name, workflow.itemCardId);
 }
