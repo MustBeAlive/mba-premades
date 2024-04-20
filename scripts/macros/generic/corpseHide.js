4f3f8df23db834e7f955c348a8a293c0fbd69733
MustBeAlive <petralexd3@gmail.com>
2024-04-20 04:29:54 +0300

    0.0.77

--- scripts/macros/generic/corpseHide.js
+++ scripts/macros/generic/corpseHide.js
@@ -1,6 +1,6 @@
 export async function corpseHide(combat, update, options, userId) {
     if (!combat.started) return;
-    let combatants = Array.from(game.combat.combatants).filter(i => i.isDefeated === true && i.isNPC === true && i.token.hidden != 1);
+    let combatants = Array.from(game.combat.combatants).filter(i => i.isDefeated === true && i.isNPC === true && i.token.hidden != 1 && !chrisPremades.helpers.findEffect(i.actor, "Regeneration"));
     if (!combatants.length) return;
     let updates = {
         token: {
@@ -21,30 +21,6 @@ export async function corpseHide(combat, update, options, userId) {
             .file("jb2a.misty_step.01.dark_black")
             .atLocation(token)
             .scaleToObject(2 * tokenDoc.texture.scaleX)
-            .aboveInterface()
-
-            .effect()
-            .file("jb2a.smoke.puff.centered.dark_black.0")
-            .delay(1000)
-            .atLocation(token)
-            .scaleToObject(3 * tokenDoc.texture.scaleX)
-            .fadeOut(500)
-            .aboveInterface()
-
-            .effect()
-            .file("jb2a.smoke.puff.centered.dark_black.1")
-            .delay(1000)
-            .atLocation(token)
-            .scaleToObject(3 * tokenDoc.texture.scaleX)
-            .fadeOut(500)
-            .aboveInterface()
-
-            .effect()
-            .file("jb2a.smoke.puff.centered.dark_black.2")
-            .delay(1000)
-            .atLocation(token)
-            .scaleToObject(3 * tokenDoc.texture.scaleX)
-            .fadeOut(500)
             .aboveInterface()
             .waitUntilFinished(-1300)
 
